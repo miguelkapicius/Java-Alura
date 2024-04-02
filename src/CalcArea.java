87e94fc10e1d586aa2ea31e99a3206ef33da848c
miguelkapicius <miguelkapicius@gmail.com>
2024-04-02 16:03:50 -0300

    Formacao em Java | Alura

---
+++ src/CalcArea.java
@@ -0,0 +1,2 @@
+public class CalcArea {
+}
---
+++ src/PositiveNegative.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Challenge {
+    public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = reading.nextInt();
+
+        if (number < 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is positive");
+        }
+    }
+}
